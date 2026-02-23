9922e2df2e00022fa7483d55c0e20324beb580bb
Saumya Maurya <saumyamaurya759@gmail.com>
2026-02-23 17:52:48 +0530

    Add Python quiz application source code.

---
+++ quiz.py
@@ -0,0 +1,117 @@
+#                               QUIZ
+print('options type in capital letters')
+print('Q.1','Which keyword is use to define a function in Python?')
+d1 = {'A':'func','B':'define','C':'def','D':'function'}
+print('A) func\nB) define\nC) def\nD) function')
+ans1 = input('enter your ans:')
+each_score = 1
+total_score = 0
+if ans1 == 'C':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.2','What is the correct file extension for Python files')
+d1 = {'A':'.pt','B':'.py','C':'.pyt','D':'.python'}
+print('A) .pt\nB).py\nC).pyt\nD).python')
+ans2 = input('enter your ans:')
+if ans2 == 'B':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.3','What does the range(5) function return?')
+d1 = {'A':'[1,2,3,4,5]','B':'[0,1,2,3,4,5]','C':'[0,1,2,3,4]','D':'[1,2,3,4]'}
+print('A) [1,2,3,4,5]\nB) [0,1,2,3,4,5]\nC) [0,1,2,3,4]\nD) [1,2,3,4]')
+ans3 = input('enter your ans:')
+if ans3 == 'C':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.4','Which one is not a programming language?')
+d1 = {'A':'HTML','B':'Python','C':'C++','D':'Java'}
+print('A) HTML\nB) Python\nC) C++\nD) Java')
+ans4 = input('enter your ans:')
+if ans4 == 'A':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.5','Which operator is used for exponentiation in Python ')
+d1 = {'A':'^','B':'**','C':'//','D':'%%'}
+print('A) ^\nB) **\nC) //\nD) %%')
+ans5 = input('enter your ans:')
+if ans5 == 'B':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.6','Which rule used in Python')
+d1 = {'A':'BODMAS','B':'MAS-DOB','C':'DAMASSE','D':'PEMDAS'}
+print('A) BODMAS\nB) MAS-DOB\nC) DAMASSE\nD) PEMDAS')
+ans6 = input('enter your ans:')
+if ans6 == 'D':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.7','Which of these is a correct variable named in python? ')
+d1 = {'A':'2name','B':'name_2','C':'@name','D':'name-2'}
+print('A) 2name\nB) name_2\nC) @name\nD) name-2')
+ans7 = input('enter your ans:')
+if ans7 == 'B':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.8','What will be the output of the following code?' )
+print('10','%','3')
+d1 = {'A':1,'B':3,'C':0,'D':10}
+print('A) 1\nB) 3\nC) 0\nD) 10')
+ans8 = input('enter your ans:')
+if ans8 == 'A':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.9','Which function is used to find the length of a list?')
+d1 = {'A':'count()','B':'length()','C':'size()','D':'len()'}
+print('A) count()\nB) length()\nC) size()\nD) len()')
+ans9 = input('enter your ans:')
+if ans9 == 'D':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+
+
+print('\nQ.10','What data type is the result of:type(5.0)')
+d1 = {'A':'int','B':'float','C':'decimal','D':'double'}
+print('A) int\nB) float\nC) decimal\nD) double')
+ans10 = input('enter your ans:')
+if ans10 == 'B':
+    print('correct✅')
+    total_score += each_score
+else:
+    print('incorrect❌\n')
+print(f'\nyour total score is: {total_score}')
+
+
+
